a717ad4f16512ba31c8cc408aa26b18482889a8d
Sherpa <1210908391@qq.com>
2026-07-16 17:44:19 +0800

    fix(ios): avoid callAsyncJavaScript crashes before iOS 18

--- flutter_inappwebview_ios/ios/flutter_inappwebview_ios/Sources/flutter_inappwebview_ios/PluginScriptsJS/CallAsyncJavaScriptBelowIOS14WrapperJS.swift
+++ flutter_inappwebview_ios/ios/flutter_inappwebview_ios/Sources/flutter_inappwebview_ios/PluginScriptsJS/CallAsyncJavaScriptBelowIOS14WrapperJS.swift
@@ -8,24 +8,42 @@
 import Foundation
 
 public class CallAsyncJavaScriptBelowIOS14WrapperJS {
+
+    public static let RESULT_MESSAGE_HANDLER_NAME = "flutterInAppWebViewCallAsyncJavaScriptResult"
+    public static let VAR_WINDOW_ID = "$IN_APP_WEBVIEW_CALL_ASYNC_JAVASCRIPT_WINDOW_ID"
     
     public static func CALL_ASYNC_JAVASCRIPT_BELOW_IOS_14_WRAPPER_JS() -> String {
         return """
         (function(obj) {
-            (async function(\(PluginScriptsUtil.VAR_FUNCTION_ARGUMENT_NAMES) {
+            var messageHandler = window.webkit.messageHandlers['\(RESULT_MESSAGE_HANDLER_NAME)'];
+            var jsonStringify = window.JSON.stringify;
+            var resultUuid = '\(PluginScriptsUtil.VAR_RESULT_UUID)';
+            var windowId = \(VAR_WINDOW_ID);
+            var sendResult = function(value, error) {
+                var message;
+                try {
+                    message = jsonStringify({
+                        'value': value === undefined ? null : value,
+                        'error': error,
+                        'resultUuid': resultUuid,
+                        'windowId': windowId
+                    });
+                } catch (serializationError) {
+                    message = jsonStringify({
+                        'value': null,
+                        'error': serializationError + '',
+                        'resultUuid': resultUuid,
+                        'windowId': windowId
+                    });
+                }
+                messageHandler.postMessage(message);
+            };
+            (async function(\(PluginScriptsUtil.VAR_FUNCTION_ARGUMENT_NAMES)) {
                 \(PluginScriptsUtil.VAR_FUNCTION_BODY)
             })(\(PluginScriptsUtil.VAR_FUNCTION_ARGUMENT_VALUES)).then(function(value) {
-                window.\(JavaScriptBridgeJS.get_JAVASCRIPT_BRIDGE_NAME()).callHandler('onCallAsyncJavaScriptResultBelowIOS14Received', {
-                    'value': value, 
-                    'error': null,
-                    'resultUuid': '\(PluginScriptsUtil.VAR_RESULT_UUID)'
-                });
+                sendResult(value, null);
             }).catch(function(error) {
-                window.\(JavaScriptBridgeJS.get_JAVASCRIPT_BRIDGE_NAME()).callHandler('onCallAsyncJavaScriptResultBelowIOS14Received', {
-                    'value': null, 
-                    'error': error + '',
-                    'resultUuid': '\(PluginScriptsUtil.VAR_RESULT_UUID)'
-                });
+                sendResult(null, error + '');
             });
             return null;
         })(\(PluginScriptsUtil.VAR_FUNCTION_ARGUMENTS_OBJ));
